4376c5822bb278849ac60e2d922e330d84e5ed69
Vo Thanh The <vothanhthepct2304@gmail.com>
2026-09-18 22:41:09 +0700

    update code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:logger/logger.dart';
-import 'package:luong_y_app/features/auth/login_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
@@ -67,7 +66,6 @@ class MyApp extends StatelessWidget {
               // return const TestAdsScreen();
 
               if (!isCompleted) {
-                return const LoginScreen();
                 return const MainNavigation();
               } else {
                 return const OnboardingScreen();
